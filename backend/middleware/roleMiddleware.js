37511c079bf98aa294a5f1e01b9a9ce06cc8e040
ParthPandey0204 <parth.pandey80045@gmail.com>
2026-07-16 21:33:59 +0530

    feat: add role authorization middleware

---
+++ backend/middleware/roleMiddleware.js
@@ -0,0 +1,15 @@
+const roleMiddleware = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: 'Authentication is required' });
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'You are not authorized to access this resource' });
+    }
+
+    return next();
+  };
+};
+
+module.exports = roleMiddleware;
